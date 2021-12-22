131778deb63f8a7ded754c6b1576932761310a2e
smlee <smlee.9807@gmail.com>
2021-12-22 16:26:17 +0900

    9강 : 데이터 변경 감지

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,8 +4,8 @@
       <img class="image" :src="onerooms[item_i].image"/>
       <h4>{{onerooms[item_i].title}}</h4>
       <p>{{onerooms[item_i].content}}</p>
-      <p><input v-model="month" ></p>
-      <p>사용자가 {{month}} 개월을 선택함 :  개월 </p>
+      <p><input type="number" v-model="month" /></p>
+      <p>사용자가 {{month}} 개월을 선택함</p>
       <p>{{month*onerooms[item_i].price}} 원</p>
      <button @click="$emit('modalClose')">닫기</button>
     </div>
@@ -25,6 +25,16 @@ export default {
   data(){
     return {
       month:1,
+    }
+  },
+  watch :{
+    /*month 변수의 데이터 변할때마다 watcher 실행됨.*/
+    month(a,b){
+      if(a>13){
+        alert('13초과 입력 불가');
+        this.month=b;
+
+      }
     }
   },
   methods :{
